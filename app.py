f09158da96e493c5a28801f042098f3b7fcc3786
Frosty <69807022+frostyyys@users.noreply.github.com>
2024-09-17 18:49:42 +0400

    Update app.py
    Removed Previous Hashed ADMIN Key.

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def frost_hash(text):
     combined_hash = (sha256_hash[16:] + shift12_hash)[:32]
     return combined_hash
 
-admin_hashed_password = "479acbd8c4004c518ed87652902287fe"
+admin_hashed_password = "ADMINPASSHASH"
 
 # Database models
 class User(db.Model):
